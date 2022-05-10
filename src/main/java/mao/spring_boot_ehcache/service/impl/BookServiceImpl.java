e4efab2b9af3f5da08a479fec57e93898641ec33
mao <1296193245@qq.com>
2022-05-10 12:22:32 +0800

    spring_boot_ehcache

---
+++ src/main/java/mao/spring_boot_ehcache/service/impl/BookServiceImpl.java
@@ -0,0 +1,83 @@
+package mao.spring_boot_ehcache.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+
+import mao.spring_boot_ehcache.dao.BookDao;
+import mao.spring_boot_ehcache.entity.Book;
+import mao.spring_boot_ehcache.service.IBookService;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.css.Counter;
+
+/**
+ * Project name(项目名称)：spring_boot_vue_and_element_UI_book_information_management_system
+ * Package(包名): mao.book_management.service.impl
+ * Class(类名): IBookServiceImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/5/9
+ * Time(创建时间)： 13:13
+ * Version(版本): 1.0
+ * Description(描述)： BookService的实现类
+ */
+
+
+@Service
+public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBookService
+{
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean saveBook(Book book)
+    {
+        return bookDao.insert(book) > 0;
+    }
+
+    @Override
+    public boolean modify(Book book)
+    {
+        return bookDao.updateById(book) > 0;
+    }
+
+    @Override
+    public boolean delete(Integer id)
+    {
+        return bookDao.deleteById(id) > 0;
+    }
+
+    @Override
+    public IPage<Book> getPage(int currentPage, int pageSize)
+    {
+        IPage<Book> page = new Page<>(currentPage, pageSize);
+        bookDao.selectPage(page, null);
+        return page;
+    }
+
+    @Override
+    public IPage<Book> getPage(int currentPage, int pageSize, Book book)
+    {
+        LambdaQueryWrapper<Book> bookLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        bookLambdaQueryWrapper.like(Strings.isNotEmpty(book.getType()), Book::getType, book.getType());
+        bookLambdaQueryWrapper.like(Strings.isNotEmpty(book.getName()), Book::getName, book.getName());
+        bookLambdaQueryWrapper.like(Strings.isNotEmpty(book.getDescription()), Book::getDescription, book.getDescription());
+        IPage<Book> page = new Page<>(currentPage, pageSize);
+        bookDao.selectPage(page, bookLambdaQueryWrapper);
+        return page;
+    }
+
+    @Override
+    @Cacheable(value = "bookSpace", key = "#id")
+    public Book getBookById(Integer id)
+    {
+        return bookDao.selectById(id);
+    }
+}
